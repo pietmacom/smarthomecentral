689a1ab58c045aaded380b24267d71bed0804d04
Your Name <you@example.com>
2023-03-17 23:24:09 +0100

    nodered init-container; generate .env for runtime

--- 02-smarthome/docker-compose-start-after.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -ex
-
-# Docs
-# - https://discourse.nodered.org/t/auto-install-packages-using-package-json/23135/4
-# - https://nodered.org/docs/user-guide/node-red-admin
-docker compose exec -it nodered /bin/sh -c 'sleep 20 && node-red admin install node-red-contrib-huemagic-fork && node-red admin install node-red-contrib-ccu'
-
